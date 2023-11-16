032fbf812ceea51c3967e2c8b27e0d48148b2e9c
edwinhati <edwinhati@outlook.com>
2023-11-17 01:22:05 +0800

    Update app port and hash password if not null

--- src/members/members.service.ts
+++ src/members/members.service.ts
@@ -8,15 +8,18 @@ export class MembersService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createMemberDto: CreateMemberDto) {
-    const saltOrRounds = 10;
-    const passwordHash = await bcrypt.hash(
-      createMemberDto.password,
-      saltOrRounds,
-    );
+    // Check if the password is not null before hashing
+    let passwordHash = null;
+    if (createMemberDto.password !== null) {
+      const saltOrRounds = 10;
+      passwordHash = await bcrypt.hash(createMemberDto.password, saltOrRounds);
+    }
 
+    // Destructure the password property and use the rest of the properties
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password, ...data } = createMemberDto;
 
+    // Use the hashed password if it is not null, otherwise, use null
     const memberData = {
       ...data,
       password: passwordHash,

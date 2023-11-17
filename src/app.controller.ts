44f3289c2dec9f0a9d461d79e66f639cd4d75959
edwinhati <edwinhati@outlook.com>
2023-11-17 13:07:18 +0800

    Add @Public decorator to uploadFile endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,11 +6,13 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
+import { Public } from './auth/auth.decorator';
 
 @Controller('')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {

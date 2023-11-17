2b3a9f67d7d42439de8b71e04f3a3086aba995f2
edwinhati <edwinhati@outlook.com>
2023-11-17 14:05:39 +0800

    Update deployment configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,11 +8,11 @@ module.exports = {
   deploy: {
     production: {
       key: 'key.pem',
-      user: 'ec2-user',
-      host: '18.143.141.153',
+      user: 'ubuntu',
+      host: '54.254.251.51',
       ref: 'origin/main',
       repo: 'https://github.com/edwinhati/propas-api',
-      path: '/home/ec2-user',
+      path: '/home/ubuntu',
       'pre-deploy-local': '',
       'post-deploy':
         'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
